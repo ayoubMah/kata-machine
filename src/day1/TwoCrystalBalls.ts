01616eb2563270cdba87df9397470136514c3a4a
ayoubMah <ayoubelmahjouby9@gmail.com>
2025-06-30 20:35:46 +0200

    Starting the Two crystalballs

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,13 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    // so we should return the small index of the 2 indexes
+    let midIndex = (breaks.length)/2;
+    let insdex1 = 0;
+    let index2 = midIndex;
+    // [0 0 0 0 0 1 1 1 1 1 1 ]
+    let i = 0;
+    let j = 0;
+
+
+    return 0;
 
 }
