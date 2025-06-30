0a7874b23731d81a008ad6eb5fb40fbccc09292c
ayoubMah <ayoubelmahjouby9@gmail.com>
2025-06-30 18:28:19 +0200

    will done fron binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,4 +1,20 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let highIndex = haystack.length;
+    let lowerIndex = 0;
+
+
+    do {
+        let midIndex = Math.floor(lowerIndex + (highIndex - lowerIndex)/2);
+        let value = haystack[midIndex];
+        if (value === needle){
+            return true
+        }else if (value > needle){
+            highIndex = midIndex;
+        }else {
+            lowerIndex = midIndex + 1;
+        }
+    }while (lowerIndex < highIndex);
+    return false;
 
 
 }
