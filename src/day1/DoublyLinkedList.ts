8fff00549c8b848395eb5c2d49fbccf48855d731
ayoubMah <ayoubelmahjouby9@gmail.com>
2025-07-02 00:04:18 +0200

    linkedList

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -1,9 +1,17 @@
+type Node<T> = {
+    value: T;
+    next?:Node<T>;
+    prev?:Node<T>;
+}
+
 export default class DoublyLinkedList<T> {
     public length: number;
+    private head?:Node<T>;
 
-    
+
 
     constructor() {
+        this.length = 0;
     }
 
     prepend(item: T): void {
@@ -19,9 +27,13 @@ export default class DoublyLinkedList<T> {
 
 }
     get(idx: number): T | undefined {
-
+        let curr = this.head;
+        for (let i = 0 ; i < idx && curr ; ++i){
+            curr = curr.next;
+        }
+        curr?.value;
 }
     removeAt(idx: number): T | undefined {
 
-}
+    }
 }
